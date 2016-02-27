f880e1a8271e687dcce006f808542c0f04ef54c2
Marina <do-you-speak@yandex.ru>
2016-02-27 17:52:01 +0300

    Simplify

--- coffee/script.coffee
+++ coffee/script.coffee
@@ -3,53 +3,36 @@ ctx = canvas.getContext("2d")
 ctx.font = "30px Arial"
 wordList = ["apple", "pear", "pineapple", "orange", "apricot", "peach", "raspberry", "strawberry"]
 alpha = "abcdefghijklmnopqrstuvwxyz"
-startX = 20
-startY = 250
 myWord = wordList[Math.floor(Math.random()*wordList.length)]
 guessed = []
 clicked = []
 lives = 7
 message = "Guess word"
 lineStart = 50
-rect = canvas.getBoundingClientRect()
-canvasLeftEdge = rect.left
-canvasRightEdge = rect.right
-canvasBottomEdge = rect.bottom
-canvasTopEdge = rect.top
-ballRadius = 17
-lineHeight = 35
-lettersInLine = 13
+# rect = canvas.getBoundingClientRect()
+# canvasTopEdge = rect.top
 step = 25
 textColor = "black"
 fillColor = "silver"
 messageColor = "#49ade9"
+messageContainer = document.getElementById("message")
+livesContainer = document.getElementById("lives")
+
+# Create alphabet
+drawAlphabet = ->
+  letterContainer = document.getElementById("lettersContainer")
+  for letter in alpha
+    span = document.createElement("span")
+    span.innerHTML = letter
+    span.classList.add("letter")
+    span.id = letter
+    letterContainer.appendChild(span)
 
 drawHiddenWord = ->
   for i in [1..myWord.length]
     ctx.moveTo(lineStart*i, 100)
     ctx.lineTo(lineStart*i+20, 100)
     ctx.stroke()
-
-drawAlphabet = ->
-  x = startX
-  y = startY
-  for letter in alpha
-    ctx.beginPath()
-    ctx.arc(x + 7, y - 7, ballRadius, 0, Math.PI*2)
-    ctx.fillStyle = textColor
-    ctx.stroke()
-    ctx.fillText(letter,x,y)
-    ctx.closePath()
-    if letter in clicked
-      ctx.beginPath()
-      ctx.arc(x + 7, y - 7, ballRadius, 0, Math.PI*2)
-      ctx.fillStyle = fillColor
-      ctx.fill()
-      ctx.closePath()
-    x += lineHeight
-    if x >= 460
-      y += 50
-      x = 20
 
 drawLetters = ->
   copyWord = myWord
@@ -60,72 +43,50 @@ drawLetters = ->
       copyWord = copyWord.replace(myWord[i-1], "0")
 
 drawMessage = ->
-  ctx.beginPath()
-  ctx.fillStyle = messageColor
-  ctx.fillText(message,20,30)
-  ctx.closePath()
+  messageContainer.innerHTML = message
+  messageContainer.style.color = messageColor
 
 drawLives = ->
-  ctx.beginPath()
-  ctx.fillStyle = messageColor
-  ctx.fillText(lives,460,30)
-  ctx.closePath()
+  livesContainer.innerHTML = lives
+  livesContainer.style.color = messageColor
 
 drawImage = ->
   img = document.getElementById("orange");
   ctx.drawImage(img,500,10)
 
 draw = ->
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  # ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawHiddenWord()
-  drawAlphabet()
   drawLetters()
   drawMessage()
   drawLives()
-  drawImage()
 
 # Helper method to count letter occurrences in string
 count = (string, char) ->
   re = new RegExp(char, "gi")
   return string.match(re).length
 
-# Listen to key press
+# Listen to click
 document.addEventListener("click", (e)->
-  pos = [e.clientX-canvasLeftEdge, e.clientY-canvasTopEdge]
-  if lives > 0 and message != "You win"
-    for i in [0..alpha.length]
-      if i < lettersInLine
-        if pos[0] in [(startX + lineHeight * i + 7 - ballRadius)..(startX + lineHeight * i + 7 + ballRadius)] and pos[1] in [(startY - 7 - ballRadius)..(startY - 7 + ballRadius)]
-          console.log alpha[i]
-          if !(alpha[i] in clicked) and !(alpha[i] in myWord)
-            lives -= 1
-            if lives == 0
-              messageColor = "#dc4949"
-              message = "You lose"
-          else if !(alpha[i] in clicked) and (alpha[i] in myWord)
-            for r in [0...count(myWord, alpha[i])]
-              guessed.push(alpha[i])
-              if guessed.length == myWord.length
-                messageColor = "#2ecc71"
-                message = "You win"
-          clicked.push(alpha[i])
-      else
-        if pos[0] in [(startX + lineHeight * (i - lettersInLine) + 7 - ballRadius)..(startX + lineHeight * (i - lettersInLine) + 7 + ballRadius)] and pos[1] in [(startY + 50 - 7 - ballRadius)..(startY + 50 - 7 + ballRadius)]
-          console.log alpha[i]
-          if !(alpha[i] in clicked) and !(alpha[i] in myWord)
-            lives -= 1
-            if lives == 0
-              messageColor = "#dc4949"
-              message = "You lose"
-          else if !(alpha[i] in clicked) and (alpha[i] in myWord)
-            for r in [0...count(myWord, alpha[i])]
-              guessed.push(alpha[i])
-              if guessed.length == myWord.length
-                messageColor = "#2ecc71"
-                message = "You win"
-          clicked.push(alpha[i])
+  if e.target.classList.contains("letter") and message != "You win" and message != "You lose"
+    clickedLetter = e.target.id
+    if !(clickedLetter in clicked) and !(clickedLetter in myWord)
+      lives -= 1
+      if lives == 0
+        messageColor = "#dc4949"
+        message = "You lose"
+    else if !(clickedLetter in clicked) and (clickedLetter in myWord)
+      for dummy_r in [0...count(myWord, clickedLetter)]
+        guessed.push(clickedLetter)
+        if guessed.length == myWord.length
+          messageColor = "#2ecc71"
+          message = "You win"
+    clicked.push(clickedLetter)
+    e.target.classList.add("disabled")
   draw()
 , false)
 
 window.onload = ->
+  drawAlphabet()
+  drawImage()
   draw()
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 (function() {
-  var alpha, ballRadius, canvas, canvasBottomEdge, canvasLeftEdge, canvasRightEdge, canvasTopEdge, clicked, count, ctx, draw, drawAlphabet, drawHiddenWord, drawImage, drawLetters, drawLives, drawMessage, fillColor, guessed, lettersInLine, lineHeight, lineStart, lives, message, messageColor, myWord, rect, startX, startY, step, textColor, wordList,
+  var alpha, canvas, clicked, count, ctx, draw, drawAlphabet, drawHiddenWord, drawImage, drawLetters, drawLives, drawMessage, fillColor, guessed, lineStart, lives, livesContainer, message, messageColor, messageContainer, myWord, step, textColor, wordList,
     indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   canvas = document.getElementById('gameCanvas');
@@ -11,10 +11,6 @@
   wordList = ["apple", "pear", "pineapple", "orange", "apricot", "peach", "raspberry", "strawberry"];
 
   alpha = "abcdefghijklmnopqrstuvwxyz";
-
-  startX = 20;
-
-  startY = 250;
 
   myWord = wordList[Math.floor(Math.random() * wordList.length)];
 
@@ -28,22 +24,6 @@
 
   lineStart = 50;
 
-  rect = canvas.getBoundingClientRect();
-
-  canvasLeftEdge = rect.left;
-
-  canvasRightEdge = rect.right;
-
-  canvasBottomEdge = rect.bottom;
-
-  canvasTopEdge = rect.top;
-
-  ballRadius = 17;
-
-  lineHeight = 35;
-
-  lettersInLine = 13;
-
   step = 25;
 
   textColor = "black";
@@ -52,6 +32,25 @@
 
   messageColor = "#49ade9";
 
+  messageContainer = document.getElementById("message");
+
+  livesContainer = document.getElementById("lives");
+
+  drawAlphabet = function() {
+    var j, len, letter, letterContainer, results, span;
+    letterContainer = document.getElementById("lettersContainer");
+    results = [];
+    for (j = 0, len = alpha.length; j < len; j++) {
+      letter = alpha[j];
+      span = document.createElement("span");
+      span.innerHTML = letter;
+      span.classList.add("letter");
+      span.id = letter;
+      results.push(letterContainer.appendChild(span));
+    }
+    return results;
+  };
+
   drawHiddenWord = function() {
     var i, j, ref, results;
     results = [];
@@ -59,37 +58,6 @@
       ctx.moveTo(lineStart * i, 100);
       ctx.lineTo(lineStart * i + 20, 100);
       results.push(ctx.stroke());
-    }
-    return results;
-  };
-
-  drawAlphabet = function() {
-    var j, len, letter, results, x, y;
-    x = startX;
-    y = startY;
-    results = [];
-    for (j = 0, len = alpha.length; j < len; j++) {
-      letter = alpha[j];
-      ctx.beginPath();
-      ctx.arc(x + 7, y - 7, ballRadius, 0, Math.PI * 2);
-      ctx.fillStyle = textColor;
-      ctx.stroke();
-      ctx.fillText(letter, x, y);
-      ctx.closePath();
-      if (indexOf.call(clicked, letter) >= 0) {
-        ctx.beginPath();
-        ctx.arc(x + 7, y - 7, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = fillColor;
-        ctx.fill();
-        ctx.closePath();
-      }
-      x += lineHeight;
-      if (x >= 460) {
-        y += 50;
-        results.push(x = 20);
-      } else {
-        results.push(void 0);
-      }
     }
     return results;
   };
@@ -111,17 +79,13 @@
   };
 
   drawMessage = function() {
-    ctx.beginPath();
-    ctx.fillStyle = messageColor;
-    ctx.fillText(message, 20, 30);
-    return ctx.closePath();
+    messageContainer.innerHTML = message;
+    return messageContainer.style.color = messageColor;
   };
 
   drawLives = function() {
-    ctx.beginPath();
-    ctx.fillStyle = messageColor;
-    ctx.fillText(lives, 460, 30);
-    return ctx.closePath();
+    livesContainer.innerHTML = lives;
+    return livesContainer.style.color = messageColor;
   };
 
   drawImage = function() {
@@ -131,13 +95,10 @@
   };
 
   draw = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawHiddenWord();
-    drawAlphabet();
     drawLetters();
     drawMessage();
-    drawLives();
-    return drawImage();
+    return drawLives();
   };
 
   count = function(string, char) {
@@ -147,73 +108,33 @@
   };
 
   document.addEventListener("click", function(e) {
-    var i, j, k, l, m, n, o, p, pos, r, ref, ref1, ref10, ref11, ref12, ref13, ref14, ref15, ref16, ref17, ref18, ref19, ref2, ref20, ref21, ref22, ref3, ref4, ref5, ref6, ref7, ref8, ref9, results, results1, results2, results3;
-    pos = [e.clientX - canvasLeftEdge, e.clientY - canvasTopEdge];
-    if (lives > 0 && message !== "You win") {
-      for (i = j = 0, ref = alpha.length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-        if (i < lettersInLine) {
-          if ((ref1 = pos[0], indexOf.call((function() {
-            results = [];
-            for (var k = ref2 = startX + lineHeight * i + 7 - ballRadius, ref3 = startX + lineHeight * i + 7 + ballRadius; ref2 <= ref3 ? k <= ref3 : k >= ref3; ref2 <= ref3 ? k++ : k--){ results.push(k); }
-            return results;
-          }).apply(this), ref1) >= 0) && (ref4 = pos[1], indexOf.call((function() {
-            results1 = [];
-            for (var l = ref5 = startY - 7 - ballRadius, ref6 = startY - 7 + ballRadius; ref5 <= ref6 ? l <= ref6 : l >= ref6; ref5 <= ref6 ? l++ : l--){ results1.push(l); }
-            return results1;
-          }).apply(this), ref4) >= 0)) {
-            console.log(alpha[i]);
-            if (!(ref7 = alpha[i], indexOf.call(clicked, ref7) >= 0) && !(ref8 = alpha[i], indexOf.call(myWord, ref8) >= 0)) {
-              lives -= 1;
-              if (lives === 0) {
-                messageColor = "#dc4949";
-                message = "You lose";
-              }
-            } else if (!(ref9 = alpha[i], indexOf.call(clicked, ref9) >= 0) && (ref10 = alpha[i], indexOf.call(myWord, ref10) >= 0)) {
-              for (r = m = 0, ref11 = count(myWord, alpha[i]); 0 <= ref11 ? m < ref11 : m > ref11; r = 0 <= ref11 ? ++m : --m) {
-                guessed.push(alpha[i]);
-                if (guessed.length === myWord.length) {
-                  messageColor = "#2ecc71";
-                  message = "You win";
-                }
-              }
-            }
-            clicked.push(alpha[i]);
-          }
-        } else {
-          if ((ref12 = pos[0], indexOf.call((function() {
-            results2 = [];
-            for (var n = ref13 = startX + lineHeight * (i - lettersInLine) + 7 - ballRadius, ref14 = startX + lineHeight * (i - lettersInLine) + 7 + ballRadius; ref13 <= ref14 ? n <= ref14 : n >= ref14; ref13 <= ref14 ? n++ : n--){ results2.push(n); }
-            return results2;
-          }).apply(this), ref12) >= 0) && (ref15 = pos[1], indexOf.call((function() {
-            results3 = [];
-            for (var o = ref16 = startY + 50 - 7 - ballRadius, ref17 = startY + 50 - 7 + ballRadius; ref16 <= ref17 ? o <= ref17 : o >= ref17; ref16 <= ref17 ? o++ : o--){ results3.push(o); }
-            return results3;
-          }).apply(this), ref15) >= 0)) {
-            console.log(alpha[i]);
-            if (!(ref18 = alpha[i], indexOf.call(clicked, ref18) >= 0) && !(ref19 = alpha[i], indexOf.call(myWord, ref19) >= 0)) {
-              lives -= 1;
-              if (lives === 0) {
-                messageColor = "#dc4949";
-                message = "You lose";
-              }
-            } else if (!(ref20 = alpha[i], indexOf.call(clicked, ref20) >= 0) && (ref21 = alpha[i], indexOf.call(myWord, ref21) >= 0)) {
-              for (r = p = 0, ref22 = count(myWord, alpha[i]); 0 <= ref22 ? p < ref22 : p > ref22; r = 0 <= ref22 ? ++p : --p) {
-                guessed.push(alpha[i]);
-                if (guessed.length === myWord.length) {
-                  messageColor = "#2ecc71";
-                  message = "You win";
-                }
-              }
-            }
-            clicked.push(alpha[i]);
+    var clickedLetter, dummy_r, j, ref;
+    if (e.target.classList.contains("letter") && message !== "You win" && message !== "You lose") {
+      clickedLetter = e.target.id;
+      if (!(indexOf.call(clicked, clickedLetter) >= 0) && !(indexOf.call(myWord, clickedLetter) >= 0)) {
+        lives -= 1;
+        if (lives === 0) {
+          messageColor = "#dc4949";
+          message = "You lose";
+        }
+      } else if (!(indexOf.call(clicked, clickedLetter) >= 0) && (indexOf.call(myWord, clickedLetter) >= 0)) {
+        for (dummy_r = j = 0, ref = count(myWord, clickedLetter); 0 <= ref ? j < ref : j > ref; dummy_r = 0 <= ref ? ++j : --j) {
+          guessed.push(clickedLetter);
+          if (guessed.length === myWord.length) {
+            messageColor = "#2ecc71";
+            message = "You win";
           }
         }
       }
+      clicked.push(clickedLetter);
+      e.target.classList.add("disabled");
     }
     return draw();
   }, false);
 
   window.onload = function() {
+    drawAlphabet();
+    drawImage();
     return draw();
   };
 
